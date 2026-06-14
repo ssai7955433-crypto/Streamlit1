2d8abae1b6de9bd7d2455744479d116e239622ea
V SHARAVANA DEVANKANDA SAI <ssai7955433@gmail.com>
2026-06-14 10:28:19 +0530

    Refactor birthday message formatting
    Removed extra line breaks and adjusted formatting for clarity.

--- birthday1.py
+++ birthday1.py
@@ -23,16 +23,12 @@ Thank you for being such an amazing friend.Your kindness,
 May this year bring you endless opportunities, good health,
     great achievements, and all the happiness you deserve. May all
     your dreams come true and may your smile always shine brightly.
-
-    No matter where life takes us, our friendship will always remain
+No matter where life takes us, our friendship will always remain
     strong. Thank you for standing by me through good times and bad.
     Friends like you are rare and precious.
-
-    Wishing you a birthday filled with love, joy, fun, and unforgettable
+Wishing you a birthday filled with love, joy, fun, and unforgettable
     moments. Enjoy your special day to the fullest!
-
-    🎂 Happy Birthday Once Again, Attitude pilla 😆❤️‍🩹💗! 🎂
-
+ 🎂 Happy Birthday Once Again, Attitude pilla 😆❤️‍🩹💗! 🎂
 ❤️ Friends Forever! ❤️
 """)
 
